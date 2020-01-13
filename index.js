98220e8e45f42c60adcf82b02591ab53cd551e05
tucker <tuckerwray84@gmail.com>
2020-01-13 18:54:36 -0500

    install dependencies from package.json + express, define consts express, server and dbMethods imports, parse repsonses to json for frontened consumabilitym set port number and listen||create initial get request fetching all user objects from the database with success and error outcomes defined

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// implement your API here
+const express = require(`express`)
+const server = express()
+const Users = require(`./data/db.js`)
+
+server.use(express.json()) //parse the responses to json for the frontend to recieve in a readible fashion
+
+server.get(`/`, function (request, response) {
+    //read data from the datasbase(Users)
+    response.send({ hey: 'servers up tuck!' })
+})
+
+//GET list of users
+//returns an array of all the user objects contained in the database
+
+
+server.get(`/api/users`, (req, res) => {
+
+    //read the data from the database Users aka ./data/db.js
+    Users.find()
+        .then(users => {
+            res.status(200).json(hubs);
+        })
+        .catch(error => {
+            console.log(error);
+            res.state(500).json({ errorMessage: "The users information could not be retrieved." })
+        });
+});
+
+const port = 7000;
+server.listen(port,()=>console.log(`\n ** api on port : ${port} ** \n`))
