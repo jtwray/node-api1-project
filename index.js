b781012abbfef53932f77acb4693e61dd27dc0fc
tucker <tuckerwray84@gmail.com>
2020-01-15 00:55:47 -0500

    set up a server and a base port to listen on . export database methods and require them in the endpoint definitions file, [index.js at this point] | establish Uniform Resource Identifiers URIs for getting a single user getting all users and creating a new user. define successful and error responses and status codes with json messages.

--- index.js
+++ index.js
@@ -18,13 +18,41 @@ server.get(`/api/users`, (req, res) => {
     //read the data from the database Users aka ./data/db.js
     Users.find()
         .then(users => {
-            res.status(200).json(hubs);
+            res.status(200).json(users);
         })
         .catch(error => {
             console.log(error);
-            res.state(500).json({ errorMessage: "The users information could not be retrieved." })
+            res.status(500).json({ errorMessage: "The users information could not be retrieved." })
         });
 });
+server.post('/api/users', (req, res) => {
+    (!req.params.name || !req.params.bio) ?
+        (res.status(400).json({ errorMessage: "Please provide name and bio for the user." }))
+        :
+        Users.insert(req.body)
+            .then(user => {
+                res.status(201).json({ user });
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ message: 'There was an error while saveing the user to the database' })
+            })
+})
+server.get('/api/users/:id', (req, res) => {
+    Users.findByid(req.params.id).then(user => {
+        user ?
+            res.status(200).json(user)
+            :
+            res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+    })
+        .catch(error => { console.log(error); res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+     });
+})
 
+
+server.delete('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id).then(count => { count > 0 ? res.status(200).json({ message: `Successfully deleted ${count} user(s)` })})
+})
+server.put('/api/users/:id')
 const port = 7000;
-server.listen(port,()=>console.log(`\n ** api on port : ${port} ** \n`))
+server.listen(port, () => console.log(`\n ** api on port : ${port} ** \n`))
